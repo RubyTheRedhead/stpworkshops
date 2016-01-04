9b835811be7d2c1c1c39efb24e37ccd93cb9bb74
Agnieszka Lasyk <agnieszkalasyk@agalasyk.local>
2016-01-04 11:39:34 +0100

    error handling

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 var express = require("express")
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello world!");
+/* app.use(function (req,res,next){
+    console.log("Time:", Date.now());
+    next();
 });
+*/
+
+function logRequest (req,res,next){
+    console.log("Time:", Date.now());
+    next();
+};
+
+function serverError(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send("server error");
+};
+
+function clientError(err, req, res, next){
+    console.error(err.stack);
+    res.status(404).send("not found");
+};
+
+
+app.get('/',logRequest, function(req, res){
+    throw new Error("jakis blad");
+    res.send("Hello world!");
+},serverError, clientError);
+
+
+
+
+
+
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('listening at port:' + 3000);
 });
