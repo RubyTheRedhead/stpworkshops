d2c3288318b64ac7bb09e688fb4743721de6ad96
Agnieszka Lasyk <agnieszkalasyk@agalasyk.local>
2016-01-07 10:03:07 +0100

    new mongo uri

--- infrastructure/base.js
+++ infrastructure/base.js
@@ -2,7 +2,7 @@ var baseConfig = {
     region: 'eu',
     maintenance: false,
     stack: 'cedar-14',
-    config_vars: { MONGOLAB_URI: 'mongodb://heroku_4x78mgjj:5c76vmaot6lpjfdubl2k6f1qc4@ds033285.mongolab.com:33285/heroku_4x78mgjj' },
+    config_vars: { MONGOLAB_URI: 'mongodb://heroku_rc6jxkln:jl60qp4df6khbm6tnh4j9139g6@ds035975.mongolab.com:35975/heroku_rc6jxkln' },
     addons: {},
     collaborators:
         [ 'lukasz.bancerowski@schibsted.pl',
